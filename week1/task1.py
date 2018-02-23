3c5475d0cd282f4dea796235673499144e491f66
MrLeylo <i.masmend@gmail.com>
2018-02-23 21:09:29 +0100

    Week1: Task 1.1

---
+++ week1/task1.py
@@ -0,0 +1,29 @@
+import os
+
+from evaluation import metrics
+
+
+def run():
+
+    tests_folder = os.path.join('datasets', 'results_testAB_changedetection',
+                                'results', 'highway')
+    gt_folder = os.path.join('datasets', 'results_testAB_changedetection',
+                             'gt', 'groundtruth')
+
+    test_a = metrics.eval_test(tests_folder, gt_folder, test_prefix='test_A_',
+                               gt_prefix='gt', test_format='png',
+                               gt_format='png', exigence=2)
+
+    test_b = metrics.eval_test(tests_folder, gt_folder, test_prefix='test_B_',
+                               gt_prefix='gt', test_format='png',
+                               gt_format='png', exigence=2)
+
+    tests = []
+    tests.append(dict(description='Test A (motion inside the region of interest'
+                                  ' as foreground)', data=test_a))
+    tests.append(dict(description='Test B (motion inside the region of interest'
+                                  ' as foreground)', data=test_b))
+
+    metrics.summarize_tests(tests)
+
+    input('Press any key to continue to the qualitative results')
