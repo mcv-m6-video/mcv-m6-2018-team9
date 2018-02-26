f523af2d6ac2c214951c472a6ed05022d6c85459
MrLeylo <i.masmend@gmail.com>
2018-02-27 00:30:41 +0100

    Task 4 added to run

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import week1
 
 def main():
     parser = argparse.ArgumentParser(description='Run the tasks of each week.')
-    parser.add_argument('week', choices=['week1', 'week2', 'week3', 'week4','week4'],
+    parser.add_argument('week', choices=['week1', 'week2', 'week3', 'week4','week5'],
                         help='Execute the tasks of a week')
 
     args = parser.parse_args()
@@ -16,6 +16,8 @@ def main():
         week1.task2.run()
         print('>>>> Executing: Week 1 - Task 3...')
         week1.task3.run()
+        print('>>>> Executing: Week 1 - Task 4...')
+        week1.task4.run()
         print('>>>> Executing: Week 1 - Task 5...')
         week1.task5.run()
 
