65b68298365de6bbd63a5cccb03ca5c1cda2fab5
Hugo Prol <hprol@innovacs.es>
2018-02-28 00:36:23 +0100

    Modify default values and images for Week1/Task5

--- week1/task5.py
+++ week1/task5.py
@@ -7,10 +7,9 @@ from video import optical_flow
 
 
 basedir = os.path.dirname(__file__)
-TEST_IMAGE_1 = os.path.join(basedir, 'resources', '000045_10.png')
-TEST_IMAGE_2 = os.path.join(basedir, 'resources', '000157_10.png')
-OPT_FLOW_1 = os.path.join(basedir, 'resources', 'gt_000045_10.png')
-OPT_FLOW_2 = os.path.join(basedir, 'resources', 'gt_000157_10.png')
+TEST_IMAGE = os.path.join(basedir, 'resources', '000157_10.png')
+OPT_FLOW_GT = os.path.join(basedir, 'resources', 'gt_000157_10.png')
+OPT_FLOW_PRED = os.path.join(basedir, 'resources', 'LKflow_000157_10.png')
 
 
 def run():
@@ -23,18 +22,13 @@ def run():
     Propose a simplification method to better visualize optical flow results.
 
     """
-    im1 = cv2.imread(TEST_IMAGE_1, cv2.IMREAD_GRAYSCALE)
-    flow1 = optical_flow.read_file(OPT_FLOW_1)
+    im = cv2.imread(TEST_IMAGE, cv2.IMREAD_GRAYSCALE)
+    gt = optical_flow.read_file(OPT_FLOW_GT)
     plt.figure()
-    plt.title("Optical flow mask (Image 45)")
-    plt.imshow(flow1[:,:,2])
-    optical_flow.plot_map(im1, flow1, size=30,
-                          title="Optical flow map (Image 45)")
-
-    im2 = cv2.imread(TEST_IMAGE_2, cv2.IMREAD_GRAYSCALE)
-    flow2 = optical_flow.read_file(OPT_FLOW_2)
-    plt.figure()
-    plt.title("Optical flow mask (Image 157)")
-    plt.imshow(flow2[:,:,2])
-    optical_flow.plot_map(im2, flow2, size=30,
-                          title="Optical flow map (Image 157)")
+    plt.title("Optical flow mask (Sequence 157 ground truth)")
+    plt.imshow(gt[:,:,2])
+    optical_flow.plot_map(im, gt, size=30,
+                          title="Optical flow (Sequence 157 ground truth)")
+    pred = optical_flow.read_file(OPT_FLOW_PRED)
+    optical_flow.plot_map(im, pred, size=30,
+                          title="Optical flow (Sequence 157 prediction)")
