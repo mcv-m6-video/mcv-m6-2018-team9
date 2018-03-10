01ee13a707e6600d983c77717992b0db45ca644a
Hugo Prol <hprol@innovacs.es>
2018-03-10 12:23:25 +0100

    Add new function imfill to morphology module

--- video/morphology.py
+++ video/morphology.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import skimage.morphology as skmorph
 
@@ -10,7 +11,6 @@ def erosion(batch, selem):
 
     Args:
       batch: numpy arrary of shape [n_images, height, width] and dtype='bool'.
-
       selem: structuring element to apply. See [1] for predefined constructors.
 
     Returns:
@@ -23,5 +23,31 @@ def erosion(batch, selem):
 
     for i, im in enumerate(batch):
         result[i] = skmorph.binary_erosion(batch[i], selem)
+
+    return result
+
+
+def imfill(batch, neighb=4):
+    """Fill the holes in a batch of binary images
+
+    Args:
+      batch: numpy arrary of shape [n_images, height, width] and dtype='bool'.
+      neighb: (int) can be 4 or 8, defines the neighbourhood connectivity which
+        determines.
+
+    Returns:
+      A numpy array with same shape as batch and dtype='bool', containing the
+      filled images.
+
+    """
+    result = np.empty_like(batch, dtype='bool')
+
+    for i, im in enumerate(batch):
+        __, cc = cv2.connectedComponents(im.astype('uint8'), neighb)
+        h, w = im.shape[:2]
+        mask = np.zeros((h + 2, w + 2), dtype='uint8')
+        cv2.floodFill(cc, mask, (0,0), 1, loDiff=0, upDiff=0,
+                      flags=cv2.FLOODFILL_MASK_ONLY)
+        result[i] = ~(mask[1:-1, 1:-1].astype('bool'))
 
     return result
