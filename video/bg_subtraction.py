d7560b6c3877dbf6e419336373c05bafe472269e
mgraugal <mgraugal@gmail.com>
2018-03-19 23:48:50 +0100

    Update bg_subtraction.py

--- video/bg_subtraction.py
+++ video/bg_subtraction.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2 as cv
-
+import numpy.ma as ma
 
 def create_model(images):
     """Compute the model parameters from a given batch of images
@@ -59,7 +59,68 @@ def predict(images, model, alpha, rho=0, return_model=False):
         return (estimation, (mean, std))
     else:
         return estimation
+    
+def create_model_mask(images, masks):
+    
+    #We create the masks for masking with numpy
+    channeled_masks = np.repeat(masks, 3)
+    
+    #True values in the mask are masked, so we invert the GT
+    masked = ma.masked_array(images.flatten(), mask=np.invert(channeled_masks.astype(bool))).reshape(ims.shape)
+    
+    mean = np.mean(masked, axis=0, dtype='float32')
+    std = np.std(masked, axis=0, dtype='float32')
+    
+    return (mean.compressed().reshape(ims.shape[1:]), std.compressed().reshape(ims.shape[1:]))
 
+def predict_masked(images, masks, model, alpha, rho=0, return_model=False):
+    """Apply background subtraction to a batch of images
+
+    Args:
+      images: numpy array with shape [num_ims, height, width, channel]
+        containing the images to process.
+      masks: numpy array with shape[num_ims, height, width] type bool 
+      with true where a pixel is valid
+      model: tuple (mean, std) obtaining with create_model() function.
+      alpha: (float) threshold which controls when a pixel is foreground
+      rho: (float) adaptive factor used to modify model as the batch of images
+        is analized.
+      return_model: (bool) if True, the final model is returned in addition to
+        the predictions.
+
+    Returns:
+      A numpy array with shape [num_ims, height, width] and dtype 'bool', with
+      the background prediction. True denotes the corresponding pixel is
+      background and False denotes it is foreground.
+
+      When return_model=True, a second element is returned with the model
+      parameters (mean, std) after analyze the images. Useful to analyze the
+      model adaptation when rho != 0.
+
+    """
+    n, h, w, _ = images.shape
+    mean = model[0].copy()
+    std = model[1].copy()
+    estimation = np.zeros((n, h, w), dtype='bool')
+
+    channeled_masks = np.repeat(masks, 3)
+    
+    #True values in the mask are masked, so we invert the GT
+    masked = ma.masked_array(images.flatten(), mask=np.invert(channeled_masks.astype(bool))).reshape(ims.shape)
+    
+    for i, im in enumerate(masked):
+        fgmask = np.absolute(im - mean) >= alpha * (std + 2)
+        estimation[i] = np.prod(fgmask, axis=-1, dtype='bool')
+
+        if rho != 0:
+            mean[~fgmask] = rho * im[~fgmask] + (1 - rho) * mean[~fgmask]
+            std[~fgmask] = np.sqrt(rho * (im[~fgmask] - mean[~fgmask])**2 +
+                                   (1 - rho) * std[~fgmask]**2)
+
+    if return_model:
+        return (estimation, (mean, std))
+    else:
+        return estimation
 
 def create_model_opencv(threshold):
     return cv.bgsegm.createBackgroundSubtractorLSBP(LSBPthreshold=threshold)
