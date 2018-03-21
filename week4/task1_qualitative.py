819d73618b554bc856359f5235c4ec6a784c7f2c
Hugo Prol <hprol@innovacs.es>
2018-03-21 03:09:40 +0100

    Update prints in week4, task1_qualitative

--- week4/task1_qualitative.py
+++ week4/task1_qualitative.py
@@ -16,22 +16,26 @@ def run():
     pred45 = optical_flow.block_matching(seq45[0], seq45[1],
                                          block_size=block_size,
                                          max_motion=max_motion)
-    msen45, __, err_im45, err_vec45 = metrics.optflow_metrics(pred45, gt45)
+    msen45, pepn45, err_im45, err_vec45 = metrics.optflow_metrics(pred45, gt45)
 
     # Sequence 157
     pred157 = optical_flow.block_matching(seq157[0], seq157[1],
                                           block_size=block_size,
                                           max_motion=max_motion)
-    msen157, __, err_im157, err_vec157 = metrics.optflow_metrics(pred157, gt157)
+    msen157, pepn157, err_im157, err_vec157 = metrics.optflow_metrics(pred157, gt157)
 
     # Plots for sequence 45
     metrics.plot_optflow_errors(err_vec45, err_im45,
                                 f'sequence #45')
     optical_flow.plot_map(seq45[0], pred45, size=block_size,
                           title='Optical flow prediction (sequence #45)')
+    print(f'MSEN for sequence 45: {msen45:0.02f}')
+    print(f'PEPN for sequence 45: {pepn45:0.02f}')
 
     # Plots for sequence 157
     metrics.plot_optflow_errors(err_vec157, err_im157,
                                 f'sequence #157')
     optical_flow.plot_map(seq157[0], pred157, size=block_size,
                           title='Optical flow prediction (sequence #157)')
+    print(f'MSEN for sequence 157: {msen157:0.02f}')
+    print(f'PEPN for sequence 157: {pepn157:0.02f}')
