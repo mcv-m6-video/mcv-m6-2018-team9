f85e7d1384bf5fd70e5d343e9304691fa467ea0a
mgraugal <mgraugal@gmail.com>
2018-04-10 12:42:28 +0200

    Update speed.py

--- video/speed.py
+++ video/speed.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 
-
-def speed(sp={}, filters = None, meter_pix=9./123.55 , fps=12, matrix = np.eye(3), out_image = None, n_frames = 0):
+    
+def speed(sp={}, filters = None, meter_pix=9.14/70.55 , fps=25, matrix = np.eye(3), out_image = None, n_frames = 0):
     """
     params:
       sp: dictionary where the speeds will be stored
@@ -13,28 +13,27 @@ def speed(sp={}, filters = None, meter_pix=9./123.55 , fps=12, matrix = np.eye(3
       out_image: Im supplied, speed will be written in the centroid position
       n_frames: if supplied, frames previous to the object appearence will have 0 speed
     
-    """
-    
+    """    
     for kfilt in filters:
         # speed estimator
         try:
 
-            trans_motion = np.dot(matrix, np.append(kfilt['motion'],np.zeros(1)) )
+            trans_motion = np.dot(matrix, np.append(kfilt['centroid']-sp[str(kfilt['id'])],np.zeros(1)) )
             dist = np.linalg.norm(trans_motion)
-            
+            print(dist)
             #pix/frame-> m/s
             speed= dist*meter_pix*fps*3.6
-            
-            sp[str(kfilt['id'])].append(speed)
+            sp['speed'+str(kfilt['id'])].append(speed)
+            sp[str(kfilt['id'])] = kfilt['centroid']
             if out_image is not None:
         
-                cv2.putText(out_image, str(sp), (kfilt['centroid'][0],kfilt['centroid'][1])
+                cv2.putText(out_image, str(speed), (kfilt['centroid'][0],kfilt['centroid'][1])
                        , cv2.FONT_HERSHEY_SIMPLEX, 0.9, 255)
 
         except KeyError:
 
-            sp[str(kfilt['id'])] = n_frames*[0]
-   
+            sp[str(kfilt['id'])] = kfilt['centroid']
+            sp['speed'+str(kfilt['id'])] = []
     if out_image is not None: 
         return out_image
     else:
